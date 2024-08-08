36433e617ccb9fbb9feead5ffae9f94152a878d3
Shengyang Zhuang <shengyangzhuang@outlook.com>
2024-08-08 18:55:50 +0100

    Updated php email form

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'heyeason@outlook.com';
+  $receiving_email_address = 'eason.s.zhuang@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
