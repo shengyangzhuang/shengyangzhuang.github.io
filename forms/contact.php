c24a278f226074174d45945bb2d225cfff0a9e95
Shengyang Zhuang <shengyangzhuang@outlook.com>
2024-08-08 18:52:16 +0100

    Added researchgate icon.

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 's.zhuang00@outlook.com';
+  $receiving_email_address = 'heyeason@outlook.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
